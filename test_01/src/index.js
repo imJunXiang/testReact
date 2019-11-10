2c86c9ef780901c6ed2a22e271ff78bd3b6a5a66
junxiang <909015430@qq.com>
2019-11-10 17:51:00 +0800

    路由

--- test_01/src/index.js
+++ test_01/src/index.js
@@ -1,68 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
-const Main = () => (
-    <main>
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/roster' component={Roster}/>
-            <Route path='/schedule' component={Schedule}/>
-        </Switch>
-    </main>
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
 )
 
-const Roster = () => (
-    <div>
-        <h2>This is a roster page!</h2>
-        <Switch>
-            <Route exact path='/roster' component={FullRoster}/>
-        {/* <Route path='/roster/:number' component={Player}/> */}
-        </Switch>
-    </div>
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
 )
 
-const FullRoster = () => (
-    <div>
-        hello world
-      {/* <ul>
-        {
-          PlayerAPI.all().map(p => (
-            <li key={p.number}>
-              <Link to={`/roster/${p.number}`}>{p.name}</Link>
-            </li>
-          ))
-        }
-      </ul> */}
-    </div>
-  )
-  
-  const Schedule = () => (
+const Topic = ({match}) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({match}) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v.State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={()=>(
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const BasicExample = () => (
+  <Router>
     <div>
       <ul>
-        <li>6/5 @ Evergreens</li>
-        <li>6/8 vs Kickers</li>
-        <li>6/14 @ United</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
       </ul>
-    </div>
-  )
-  
-  const Home = () => (
-    <div>
-      <h1>Welcome to the Tornadoes Website!</h1>
-    </div>
-  )
 
-const App = function App() {
-    return (
-        <div>
-            <Main/>
-        </div>
-    )
-}
+      <hr/>
 
-ReactDOM.render((
-    <BrowserRouter>
-        <App></App>
-    </BrowserRouter>
-),document.getElementById('root'))
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(
+  <BasicExample />,
+  document.getElementById("root")
+);
